ab5fa1fd203378b98a675616a2b1f92841017e07
Andrew Grosner <andrew.grosner@raizlabs.com>
2015-01-13 11:17:14 -0500

    added comments to this class and added a customization option

--- src/main/java/com/raizlabs/android/debugmodule/view/NoContentDrawerLayout.java
+++ src/main/java/com/raizlabs/android/debugmodule/view/NoContentDrawerLayout.java
@@ -4,24 +4,40 @@ import android.content.Context;
 import android.support.v4.widget.DrawerLayout;
 import android.util.AttributeSet;
 import android.util.TypedValue;
-import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 
 /**
- * Description:
+ * Description: Lets touches fall through that are not explicit drawer right swipes. This is so
+ * we can have a {@link android.support.v4.widget.DrawerLayout} on top of the main content view of the root.
  */
 public class NoContentDrawerLayout extends DrawerLayout {
 
+    /**
+     * The width of the screen, in pixels
+     */
     int screenWidth;
 
+    /**
+     * The vertical touch width allowed for th {@link android.support.v4.widget.DrawerLayout}. It
+     * shrinks and expands when the drawer slides. It shrinks so we can allow touches to the attached activity.
+     */
     int drawerSide;
 
+    /**
+     * If true, we are allow all touches as normal to the {@link android.support.v4.widget.DrawerLayout}
+     */
     boolean isDraggingDrawer = false;
 
-    boolean isDrawerOpen = true;
+    /**
+     * If true, we allow all touches as normal to the {@link android.support.v4.widget.DrawerLayout}
+     */
+    boolean isDrawerOpen = false;
 
-    int defaultSideSize;
+    /**
+     * The minimum touch size in pixels.
+     */
+    int minimumTouchSize;
 
     public NoContentDrawerLayout(Context context) {
         super(context);
@@ -38,21 +54,37 @@ public class NoContentDrawerLayout extends DrawerLayout {
         init(context);
     }
 
+    /**
+     * Called when this class is created. Sets up our defaults.
+     *
+     * @param context The context of the activity.åß
+     */
     protected void init(Context context) {
         screenWidth = context.getResources().getDisplayMetrics().widthPixels;
-        defaultSideSize = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50, context.getResources().getDisplayMetrics());
-        drawerSide = defaultSideSize;
+        minimumTouchSize = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50, context.getResources().getDisplayMetrics());
+        drawerSide = minimumTouchSize;
         setDrawerListener(mListener);
+    }
+
+    /**
+     * Sets the minimum touch size of the debug drawer, in pixels.
+     * @param touchSizePixels
+     */
+    public void setMinimumTouchSize(int touchSizePixels) {
+        minimumTouchSize = touchSizePixels;
+        if (drawerSide < minimumTouchSize) {
+            drawerSide = minimumTouchSize;
+        }
     }
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
         float x = ev.getX();
 
-        if(ev.getActionMasked() == MotionEvent.ACTION_UP) {
+        if (ev.getActionMasked() == MotionEvent.ACTION_UP) {
             isDraggingDrawer = false;
         }
-        if(isDrawerOpen || isDraggingDrawer || screenWidth-x < drawerSide) {
+        if (isDrawerOpen || isDraggingDrawer || screenWidth - x < drawerSide) {
             isDraggingDrawer = true;
             return super.onInterceptTouchEvent(ev);
         } else {
@@ -64,10 +96,10 @@ public class NoContentDrawerLayout extends DrawerLayout {
     public boolean onTouchEvent(MotionEvent ev) {
         float x = ev.getX();
 
-        if(ev.getActionMasked() == MotionEvent.ACTION_UP) {
+        if (ev.getActionMasked() == MotionEvent.ACTION_UP) {
             isDraggingDrawer = false;
         }
-        if(isDrawerOpen || isDraggingDrawer || screenWidth-x < drawerSide) {
+        if (isDrawerOpen || isDraggingDrawer || screenWidth - x < drawerSide) {
             isDraggingDrawer = true;
             return super.onTouchEvent(ev);
         } else {
@@ -75,12 +107,19 @@ public class NoContentDrawerLayout extends DrawerLayout {
         }
     }
 
+    /**
+     * Listens for slide events and shrinks and grows the touch target of this view as the user
+     * drags the drawer. It is so we can adequately swipe open the drawer and enable touches to "fall through"
+     * to the main content area at the same time.
+     */
     private final DrawerListener mListener = new DrawerListener() {
         @Override
         public void onDrawerSlide(View drawerView, float slideOffset) {
-            drawerSide = (int) (((float)screenWidth)*slideOffset);
-            if(drawerSide < defaultSideSize) {
-                drawerSide = defaultSideSize;
+            drawerSide = (int) (((float) screenWidth) * slideOffset);
+
+            // minimum touch size
+            if (drawerSide < minimumTouchSize) {
+                drawerSide = minimumTouchSize;
             }
         }
 
